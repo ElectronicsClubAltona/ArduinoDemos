ff4d79f85d2051d21a5b987d6452678c64c61a08
Mike J H <matrixmike@icloud.com>
2016-09-15 21:26:42 +1000

    added FadeRGB

---
+++ FadeRGB/FadeRGB.ino
@@ -0,0 +1,48 @@
+/*
+  Fade
+
+  This example shows how to fade an LED on pin 9
+  using the analogWrite() function.
+
+  The analogWrite() function uses PWM, so if
+  you want to change the pin you're using, be
+  sure to use another PWM capable pin. On most
+  Arduino, the PWM pins are identified with
+  a "~" sign, like ~3, ~5, ~6, ~9, ~10 and ~11.
+
+  15 September 2016
+  This cycles three primary colours - one at a time
+*/
+
+int led = 9;           // the PWM pin the LED is attached to
+int brightness = 0;    // how bright the LED is
+int fadeAmount = 5;    // how many points to fade the LED by
+
+// the setup routine runs once when you press reset:
+void setup() {
+  // declare pin 9 to be an output:
+  pinMode(led, OUTPUT);
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  delay(30);
+  // set the brightness of pin 9:
+  analogWrite(led, brightness);
+
+  // change the brightness for next time through the loop:
+  brightness = brightness + fadeAmount;
+
+  // reverse the direction of the fading at the ends of the fade:
+  if (brightness <= 0 || brightness >= 255) {
+    fadeAmount = -fadeAmount;
+  }
+  if (brightness <= 0 ) {
+    led = led + 1;
+    if (led > 11) { // really a modulo effect
+      led = 9;
+    }
+  }
+}
+
+
