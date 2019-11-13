ebebd3caa2ac3f6f23c44a00503a40b0b48627c1
Mike <matroxmike@icloud.com>
2019-11-14 00:59:11 +1100

    using DK Electronics v1.0 old motor shield a

--- MotorParty1/MotorParty1.ino
+++ MotorParty1/MotorParty1.ino
@@ -1,9 +1,9 @@
-// Adafruit Motor shield library
-// copyright Adafruit Industries LLC, 2009
+// Ada-fruit Motor shield library
+// copyright Ada-fruit Industries LLC, 2009
 // this code is public domain, enjoy!
 
 #include <AFMotor.h>
-#include <Servo.h> 
+#include <Servo.h>
 
 // DC motor on M2
 AF_DCMotor motor(2);
@@ -15,10 +15,10 @@ AF_Stepper stepper(48, 2);
 void setup() {
   Serial.begin(9600);           // set up Serial library at 9600 bps
   Serial.println("Motor party!");
-  
+
   // turn on servo
   servo1.attach(9);
-   
+
   // turn on motor #2
   motor.setSpeed(200);
   motor.run(RELEASE);
@@ -29,32 +29,32 @@ int i;
 // Test the DC motor, stepper and servo ALL AT ONCE!
 void loop() {
   motor.run(FORWARD);
-  for (i=0; i<255; i++) {
+  for (i = 0; i < 255; i++) {
     servo1.write(i);
-    motor.setSpeed(i);  
+    motor.setSpeed(i);
     stepper.step(1, FORWARD, INTERLEAVE);
     delay(3);
- }
- 
-  for (i=255; i!=0; i--) {
-    servo1.write(i-255);
-    motor.setSpeed(i);  
+  }
+
+  for (i = 255; i != 0; i--) {
+    servo1.write(i - 255);
+    motor.setSpeed(i);
     stepper.step(1, BACKWARD, INTERLEAVE);
     delay(3);
- }
- 
+  }
+
   motor.run(BACKWARD);
-  for (i=0; i<255; i++) {
+  for (i = 0; i < 255; i++) {
     servo1.write(i);
-    motor.setSpeed(i);  
+    motor.setSpeed(i);
     delay(3);
     stepper.step(1, FORWARD, DOUBLE);
- }
- 
-  for (i=255; i!=0; i--) {
-    servo1.write(i-255);
-    motor.setSpeed(i);  
+  }
+
+  for (i = 255; i != 0; i--) {
+    servo1.write(i - 255);
+    motor.setSpeed(i);
     stepper.step(1, BACKWARD, DOUBLE);
     delay(3);
- }
+  }
 }
